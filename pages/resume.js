69ba8ab6f20fc62a10844877116cee4786df3b77
SinshiSmith <yorfnd@gmail.com>
2023-01-02 17:54:05 +0200

    Resume v1.1 (#6)
    * Remove comments
    
    * Fix phone and email links
    
    * Resume.css: remove comments
    
    * Remove dead code
    
    * Add Education section

--- pages/resume.js
+++ pages/resume.js
@@ -23,16 +23,16 @@ export default function Resume() {
             <CompactContactLine href="/" name="sinshi.dev" icon={<TbWorld />} />
             <CompactContactLine
               href="/"
-              name="Egypt, Cairo"
+              name="Giza, Egypt"
               icon={<MdLocationPin />}
             />
             <CompactContactLine
-              href="/"
+              href="tel:+201110763080"
               name="+201110763080"
               icon={<FaWhatsapp />}
             />
             <CompactContactLine
-              href="/"
+              href="mailto:yorfnd@gmail.com"
               name="yorfnd@gmail.com"
               icon={<FaEnvelope />}
             />
@@ -43,83 +43,65 @@ export default function Resume() {
             />
           </div>
         </div>
-        <div className={styles.resume}>
-          <main className={styles.main}>
-            <section>
-              <h2>Experience:</h2>
-              <Experience
-                company="Pocketful of Quarters, Inc"
-                date="2020-Current"
-                items={[
-                  `Leading a team of 5+ developers for migration into a new
+
+        <section>
+          <h2>Experience:</h2>
+          <Experience
+            company="Pocketful of Quarters, Inc"
+            date="2020-Current"
+            items={[
+              `Leading a team of 5+ developers for migration into a new
                 database scheme covered with Integration and e2e tests`,
-                  `Working closely with CEO and users to Revamp the website UX/Ul design`,
-                  `Creating mock kit to make unit and integration tests easier and faster`,
+              `Working closely with CEO and users to Revamp the website UX/Ul design`,
+              `Creating mock kit to make unit and integration tests easier and faster`,
 
-                  `Designing a transactions page for the blockchain currency
+              `Designing a transactions page for the blockchain currency
                   Quarters`,
-                ]}
-              />
-              <Experience
-                company="Tatsu Works Pte Ltd"
-                date="2019-2020"
-                items={[
-                  `Creating a scalable and reliable code base from scratch using tech stack that work best for the website`,
-                  `Implementing real-time Discord embedded message
+            ]}
+          />
+          <Experience
+            company="Tatsu Works Pte Ltd"
+            date="2019-2020"
+            items={[
+              `Creating a scalable and reliable code base from scratch using tech stack that work best for the website`,
+              `Implementing real-time Discord embedded message
                 preview`,
-                  `Implementing an in-app store for digital goods with search, categories and filtering`,
-                  `Implementing discord bot management dashboard`,
-                ]}
-              />
-            </section>
-          </main>
-          <side>
-            {/* <section>
-              <h2>Contact Info:</h2>
-              <ContactLine name="Email" value="yorfnd@gmail.com" href="" />
-              <ContactLine
-                name="Website"
-                value="sinshi.dev"
-                href="https://www.sinshi.dev"
-              />
-              <ContactLine
-                name="Github"
-                value="github.com/SinshiSmith"
-                href="https://github.com/SinshiSmith"
-              />
-            </section> */}
-            <section>
-              <h2>Skills:</h2>
-              <ul>
-                <li>HTML, CSS, JavaScript</li>
-                <li>React: Next.js, Gatsby</li>
-                <li>CSS-IN-JS: emotion.js, styled-components</li>
-                <li>Restful APIs: Axios</li>
-                <li>Graphql: Relay.js, Apollo</li>
-                <li>E2E tests: Cypress, Playwright</li>
-                <li>Unit/Integration tests: react-testing-library</li>
-                <li>Git: github, CI/CD, code reviews</li>
-              </ul>
-            </section>
-            <section>
-              <h2>languages:</h2>
-              <ul>
-                <li>Arabic: Native</li>
-                <li>English: Fluent</li>
-              </ul>
-            </section>
-          </side>
-        </div>
-      </div>
-    </div>
-  );
-}
+              `Implementing an in-app store for digital goods with search, categories and filtering`,
+              `Implementing discord bot management dashboard`,
+            ]}
+          />
+        </section>
 
-function ContactLine({ name, value, href }) {
-  return (
-    <div className={styles.contactLine}>
-      <p>{name}</p>
-      <Link href={href}>{value}</Link>
+        <section className={styles.experience}>
+          <h2>Education</h2>
+          <div className={styles.position}>
+            <h3>Helwan University</h3>
+            <p>2015-2019</p>
+          </div>
+          <p>Bachelor of Commerce, Applied Statistics</p>
+        </section>
+
+        <section>
+          <h2>Skills:</h2>
+          <ul>
+            <li>HTML, CSS, JavaScript</li>
+            <li>React: Next.js, Gatsby</li>
+            <li>CSS-IN-JS: emotion.js, styled-components</li>
+            <li>Restful APIs: Axios</li>
+            <li>Graphql: Relay.js, Apollo</li>
+            <li>E2E tests: Cypress, Playwright</li>
+            <li>Unit/Integration tests: react-testing-library</li>
+            <li>Git: github, CI/CD, code reviews</li>
+          </ul>
+        </section>
+        <section>
+          <h2>languages:</h2>
+          <ul>
+            <li>Arabic: Native</li>
+            <li>English: Fluent</li>
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
