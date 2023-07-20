cd98f5e3de58f3b61ca81e86c3ee049574f900e6
SinshiSmith <yorfnd@gmail.com>
2023-07-20 22:30:46 +0300

    SkillsSection: Switch to tailwind

--- components/HomePage/SkillsSection.js
+++ components/HomePage/SkillsSection.js
@@ -1,4 +1,3 @@
-import styles from "./SkillsSection.module.css";
 import {
   SiNextdotjs,
   SiHtml5,
@@ -14,23 +13,35 @@ import {
 const iconWidth = "3em";
 
 const skills = [
-  { name: "HTML", icon: <SiHtml5 size={iconWidth} /> },
-  { name: "CSS", icon: <SiCss3 size={iconWidth} /> },
-  { name: "JavaScript", icon: <SiJavascript size={iconWidth} /> },
-  { name: "React", icon: <SiReact size={iconWidth} /> },
-  { name: "Next.js", icon: <SiNextdotjs size={iconWidth} /> },
-  { name: "GraphQL", icon: <SiGraphql size={iconWidth} /> },
-  { name: "Node.js", icon: <SiNodedotjs size={iconWidth} /> },
-  { name: "PostgreSQL", icon: <SiPostgresql size={iconWidth} /> },
-  { name: "Rust", icon: <SiRust size={iconWidth} /> },
+  { name: "HTML", icon: <SiHtml5 className="m-auto" size={iconWidth} /> },
+  { name: "CSS", icon: <SiCss3 className="m-auto" size={iconWidth} /> },
+  {
+    name: "JavaScript",
+    icon: <SiJavascript className="m-auto" size={iconWidth} />,
+  },
+  { name: "React", icon: <SiReact className="m-auto" size={iconWidth} /> },
+  {
+    name: "Next.js",
+    icon: <SiNextdotjs className="m-auto" size={iconWidth} />,
+  },
+  { name: "GraphQL", icon: <SiGraphql className="m-auto" size={iconWidth} /> },
+  {
+    name: "Node.js",
+    icon: <SiNodedotjs className="m-auto" size={iconWidth} />,
+  },
+  {
+    name: "PostgreSQL",
+    icon: <SiPostgresql className="m-auto" size={iconWidth} />,
+  },
+  { name: "Rust", icon: <SiRust className="m-auto" size={iconWidth} /> },
 ];
 
 export default function SkillsSection() {
   return (
-    <section className={styles.section}>
+    <section className="text-center min-h-screen flex items-center justify-center">
       <div>
-        <h2>Skills And Technologies</h2>
-        <ul className={styles.grid}>
+        <h2 className="text-2xl font-bold my-5">Skills And Technologies</h2>
+        <ul className="grid gap-8 grid-cols-2 p-5 w-screen max-w-md md:grid-cols-3 md:p-0">
           {skills.map(({ name, icon }) => (
             <Skill key={name} name={name} icon={icon} />
           ))}
@@ -42,10 +53,10 @@ export default function SkillsSection() {
 
 const Skill = ({ name, icon }) => {
   return (
-    <div className={styles.skillBox}>
+    <div className="h-32 rounded bg-zinc-800 flex items-center justify-center">
       <div>
         {icon}
-        <p>{name}</p>
+        <p className="mt-2">{name}</p>
       </div>
     </div>
   );
