0c32acee71f24996124adaaa06d64b67b6d61c5c
SinshiSmith <yorfnd@gmail.com>
2025-05-11 14:00:09 +0300

    Resume Update 11 May (#19)

--- pages/resume.js
+++ pages/resume.js
@@ -1,8 +1,4 @@
 import Link from "next/link";
-import { FaEnvelope, FaWhatsapp, FaGithub } from "react-icons/fa";
-import { TbWorld } from "react-icons/tb";
-import { MdLocationPin } from "react-icons/md";
-
 import Head from "next/head";
 import Skills from "../components/Resume/Skills";
 
@@ -14,12 +10,19 @@ export default function Resume() {
         <meta name="description" content="Ahmed Hafez | Resume" />
       </Head>
       <div className="max-w-[800px] rounded bg-gray-100 p-6 text-black">
-        <div className="mb-10 flex flex-wrap justify-between">
-          <div>
-            <h1 className="text-6xl font-bold">Ahmed Hafez</h1>
-            <h2 className="text-3xl font-bold">Senior Frontend Developer</h2>
+        <div className="mb-10 flex flex-wrap justify-center">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold">Ahmed Hafez</h1>
+            {/* <h2 className="text-3xl font-bold">Senior Fullstack Engineer</h2> */}
+            <div className="flex flex-wrap gap-1">
+              <p>yorfnd@gmail.com</p>
+              <p>|</p>
+              <p>+201110763080</p>
+              <p>|</p>
+              <p>Giza, Egypt</p>
+            </div>
           </div>
-          <div>
+          {/* <div>
             <CompactContactLine href="/" name="sinshi.dev" icon={<TbWorld />} />
             <CompactContactLine
               href="/"
@@ -41,23 +44,40 @@ export default function Resume() {
               name="github.com/SinshiSmith"
               icon={<FaGithub />}
             />
-          </div>
+          </div> */}
         </div>
 
         <section>
-          <h2 className="mb-5 text-center text-2xl font-bold">
-            Professional Experience
-          </h2>
-          <hr className="my-2 border-black" />
+          <h2 className="text-2xl font-bold">WORK EXPERIENCE</h2>
+          <hr className="mb-4 border-black" />
+          {/* <Experience
+            title="Senior Software Engineer"
+            company="BME - eSports"
+            date="2023-2024"
+            items={[
+              "Built League of Legends Champion Select API for 'GameMode by WE'",
+              "Built League of Legend In-game HUD API for 'GameMode by WE'",
+              "Built League of Legends Champion Select API for 'Superdome Cairo 2023'",
+              "Built League of Legends In-game HUD API for 'Superdome Cairo 2023'",
+            ]}
+            skills={[
+              "League Broadcast",
+              "API Integration",
+              "OBS",
+              "vMix",
+              "CSS",
+              "React",
+            ]}
+          /> */}
           <Experience
-            title="Senior Next.js Developer"
-            company="yes!devs GmbH"
+            title="Senior Fullstack Engineer"
+            company="yes!devs GmbH, Hamburg, Germany"
             date="Nov 2023 - Current"
             items={[
+              "Integrated External APIs: Softgarden, SAP CDC, Dropbox, Contentful, Laravel",
               "Reviewed and mentored 3 Mid-level developers on best practices of web development, Next.js, and React",
               "Developed and implemented highly flexiable content modal for CMS",
               "Built and maintained websites for some of the largest companies in Germany",
-              "",
             ]}
             skills={[
               "Next.js",
@@ -70,12 +90,12 @@ export default function Resume() {
             ]}
           />
           <Experience
-            title="Senior Full-Stack Developer"
-            company="2B Creative"
+            title="Senior FullStack Engineer"
+            company="2B Creative, London, United Kingdom"
             date="Jan 2023 - Nov 2023"
             items={[
               `Developed and maintained 3 React.js websites with i18n support`,
-              `Integrated Storyblok CMS with React website to allow building web pages with visual editor`,
+              `Integrated Storyblok CMS API with React website to allow building web pages with visual editor`,
               `Improved Typescript developer experience with Storyblok`,
               `Added support for live chat & reCaptcha protection on forms`,
             ]}
@@ -90,14 +110,14 @@ export default function Resume() {
             ]}
           />
           <Experience
-            title="Lead Frontend Developer"
-            company="Pocketful of Quarters, Inc"
+            title="Lead Frontend Engineer"
+            company="Pocketful of Quarters Inc, Taxes, United States"
             date="Jan 2021 - Sep 2022"
             items={[
-              "developed an esports streamer tournaments hosting platform to serve 10s of thousands of players",
+              "Developed an eSports streamer tournaments hosting platform to serve tens of thousands of players",
               `Lead a team of 4 junior developers to migrate to a new database scheme with Integration/E2E tests`,
               `Reviewed Junior Developers PRs and Guided them to best practices for JavaScript, React, and CSS`,
-              `Collaborated with senior developers and product manager to create efficient and effective workflow using agile methodology Kanban`,
+              `Collaborated with senior developers and product manager to create efficient and effective workflow using Agile methodology Kanban`,
               `Developed mock-kit of Restful APIs for unit and integration tests`,
               `Designed and Developed a transactions page for the blockchain currency Quarters`,
             ]}
@@ -110,8 +130,8 @@ export default function Resume() {
             ]}
           />
           <Experience
-            title="Frontend Developer"
-            company="Pocketful of Quarters, Inc"
+            title="Frontend Engineer"
+            company="Pocketful of Quarters Inc, Taxes, United States"
             date="May 2020 - Jan 2021"
             items={[
               `Refactored legacy code to modern React 16.8+`,
@@ -125,7 +145,7 @@ export default function Resume() {
           />
           <Experience
             title="UX/UI Designer"
-            company="Pocketful of Quarters, Inc"
+            company="Pocketful of Quarters Inc, Taxes, United States"
             date="May 2020 - Sep 2021"
             items={[
               `Redesigned UI for existing components`,
@@ -136,8 +156,8 @@ export default function Resume() {
             ]}
           />
           <Experience
-            title="Frontend Developer"
-            company="Tatsu Works Pte Ltd"
+            title="Frontend Engineer"
+            company="Tatsu Works Pte Ltd, Singapore"
             date="Jun 2019 - May 2020"
             items={[
               `Created a scalable and reliable code base from scratch using tech stack that work best for the website`,
@@ -148,17 +168,18 @@ export default function Resume() {
           />
 
           <Experience
-            title="Frontend Developer Intern"
+            title="Frontend Engineer Intern, Cairo, Egypt"
             company="Midrule"
             date="Jun 2018 - Jul 2018"
             items={[
+              `Developed websites using HTML, CSS, Javascript, Bootstrap`,
+              "Assisted Senior Developers with their tasks",
               `Designed wireframes of UX/UI for company website`,
-              `Developed Company Website using HTML, CSS, Javascript, Bootstrap`,
             ]}
           />
         </section>
 
-        <section className="mb-5">
+        <section className="mt-10 mb-5">
           <h2 className="mb-5 text-center text-2xl font-bold">
             Technical Skills
           </h2>
@@ -173,7 +194,7 @@ export default function Resume() {
             <li>English: Fluent</li>
           </ul>
         </section>
-        <section className="mb-10">
+        {/* <section className="mb-10">
           <h2 className="mb-5 text-center text-2xl font-bold">Education</h2>
           <hr className="my-2 border-black" />
           <div className="flex flex-wrap justify-between">
@@ -183,7 +204,7 @@ export default function Resume() {
           <p className="my-4">
             Bachelor of Commerce and Business Administration, Applied Statistics
           </p>
-        </section>
+        </section> */}
       </div>
     </div>
   );
@@ -200,17 +221,29 @@ function CompactContactLine({ name, href, icon }) {
   );
 }
 
-function Experience({ company, date, items, title, skills = [] }) {
+function Experience({
+  company,
+  date,
+  items,
+  title,
+  skills = [],
+  className,
+  location,
+}) {
   return (
-    <div className="mb-10">
-      <div className="flex flex-wrap justify-between">
+    <div className={`mb-5 ${className}`}>
+      <div className="flex flex-wrap justify-between italic">
         <div>
-          <h3 className="text-lg font-bold leading-none">{title}</h3>
-          <p>{company}</p>
+          <h3 className="font-bold">{company}</h3>
+          <h3>{title}</h3>
         </div>
-        <p>{date}</p>
+
+        <div>
+          <p>{location}</p>
+          <p>{date}</p>
+        </div>
       </div>
-      {skills.length > 0 && (
+      {/* {skills.length > 0 && (
         <div className="flex flex-wrap gap-2">
           <p className="font-bold">skills:</p>
           {skills.map((skill, index) => (
@@ -219,9 +252,9 @@ function Experience({ company, date, items, title, skills = [] }) {
             </p>
           ))}
         </div>
-      )}
+      )} */}
       {items && (
-        <ul className="mb-5 mt-3 list-outside list-disc pl-5">
+        <ul className="mt-1 mb-5 list-outside list-disc pl-7">
           {items.map((item) => (
             <li key={item}>{item}</li>
           ))}
